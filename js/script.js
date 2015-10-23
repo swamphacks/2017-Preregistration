f3d6a9169c15ce4c0e84d2da034cde13432b3a53
Cody Fitzpatrick <cody@orbitalone.com>
2015-10-23 10:21:38 -0400

    Design Tweaks
    Updated logo size, subheader, marston image, and scroll position for
    menu bar appearance.

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ $(function(){
 	// Prevent elements from resizing immediately upon page load (by removing "preload" class from body upon page load)
 	$("body").removeClass("preload");	
 	// Show/hide the menu bar based on scrolling position
-	var vh = $(window).height();
+	var vh = $(window).height() - 200;
 	$(".navbar").hide();
 	$(function () {
 		$(window).scroll(function () {
