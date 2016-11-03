6900729625753519afca39fd398afb07311e1136
brownac <austincbrown@ufl.edu>
2016-11-03 19:22:04 -0400

    Email validation...don't know why I didn't do this earlier.

--- database.js
+++ database.js
@@ -13,7 +13,7 @@ $(function() {
 
   function verifyEmail(auth, actionCode) {
   // Try to apply the email verification code.
-    auth.applyActionCode(actionCode).then(function(resp) {
+  auth.applyActionCode(actionCode).then(function(resp) {
     // Email address has been verified.
     console.log('it is lit');
     // You could also provide the user with a link back to the app.
@@ -38,34 +38,36 @@ $(function() {
     event.preventDefault();
     var email = $("#email").val();
     var pass = makePass();
-    console.log(pass);
-    firebase.database().ref('subscribed-users').push({
-      email
-    }).then(() =>{
-      $('.submitted').text( "Thanks for subscribing!  Check your e-mail for confirmation." ).show();
-      $('#subscribe').css(
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(email == "" || !email.match(re)) {
+      $('.submitted').text( "Please enter an e-mail address." );
+    }
+    else {
+      firebase.database().ref('subscribed-users').push({
+        email
+      }).then(() =>{
+        $('.submitted').text( "Thanks for subscribing!  Check your e-mail for confirmation." ).show();
+        $('#subscribe').css(
         //'color', '#fff',
         'background-color', '#4d94ff' 
-      );
-    }).then(() => {
-      auth.createUserWithEmailAndPassword(email, pass);
-    }).catch(e => { console.log(e); });
+        );
+      }).then(() => {
+        auth.createUserWithEmailAndPassword(email, pass);
+      }).catch(e => { console.log(e); });
 
-    firebase.auth().onAuthStateChanged(firebaseUser => {
-      if (firebaseUser) {
-        firebaseUser.sendEmailVerification();
-        console.log('the email sent');
-        firebase.auth().signOut();
-        console.log('signed out');
-      } else {
-        console.log('something is wrong');
-      }
-    });
-
+      firebase.auth().onAuthStateChanged(firebaseUser => {
+        if (firebaseUser) {
+          firebaseUser.sendEmailVerification();
+          console.log('the email sent');
+          firebase.auth().signOut();
+          console.log('signed out');
+        } else {
+          console.log('something is wrong');
+        }
+      });
+     }
   });
 
   //check if the page is refreshed, log the current user out
-  $(window).bind('beforeunload', function() {
-  });
 
 });
